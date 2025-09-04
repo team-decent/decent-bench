fcb698f6b7e8963ca8068fd468ba7354c6d63dbf
elramen <158566966+elramen@users.noreply.github.com>
2025-09-04 19:26:45 +0200

    feat(costs): Add logistic regression (#38)
    Add logistic regression cost function and fix some docstring reference mistakes

--- decent_bench/library/core/cost_functions.py
+++ decent_bench/library/core/cost_functions.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+import copy
 from abc import ABC, abstractmethod
 
 import numpy as np
 from numpy import float64
 from numpy.typing import NDArray
+from scipy import special
 
 from decent_bench.library.core import centralized_algorithms as ca
 
@@ -128,7 +130,7 @@ class QuadraticCost(CostFunction):
 
         where :math:`\lambda_i` are the eigenvalues of :math:`\frac{1}{2} (\mathbf{A}+\mathbf{A}^T)`.
 
-        For the general definition, see :meth:`CostFunction.m_smooth`.
+        For the general definition, see :attr:`CostFunction.m_smooth`.
         """
         eigs = np.linalg.eigvalsh(self.A_sym)
         return float(np.max(np.abs(eigs)))
@@ -147,7 +149,7 @@ class QuadraticCost(CostFunction):
 
         where :math:`\lambda_i` are the eigenvalues of :math:`\frac{1}{2} (\mathbf{A}+\mathbf{A}^T)`.
 
-        For the general definition, see :meth:`CostFunction.m_cvx`.
+        For the general definition, see :attr:`CostFunction.m_cvx`.
         """
         eigs = np.linalg.eigvalsh(self.A_sym)
         l_min = float(np.min(eigs))
@@ -255,7 +257,7 @@ class LinearRegressionCost(CostFunction):
 
         where :math:`\lambda_i` are the eigenvalues of :math:`\mathbf{A}^T \mathbf{A}`.
 
-        For the general definition, see :meth:`CostFunction.m_smooth`.
+        For the general definition, see :attr:`CostFunction.m_smooth`.
         """
         return self.quadratic_cf.m_smooth
 
@@ -273,7 +275,7 @@ class LinearRegressionCost(CostFunction):
 
         where :math:`\lambda_i` are the eigenvalues of :math:`\mathbf{A}^T \mathbf{A}`.
 
-        For the general definition, see :meth:`CostFunction.m_cvx`.
+        For the general definition, see :attr:`CostFunction.m_cvx`.
         """
         return self.quadratic_cf.m_cvx
 
@@ -341,6 +343,101 @@ class LinearRegressionCost(CostFunction):
         return SumCost([self, other])
 
 
+class LogisticRegressionCost(CostFunction):
+    r"""
+    Logistic regression cost function.
+
+    .. math:: f(\mathbf{x}) =
+        -\left[ \mathbf{b}^T \log( \sigma(\mathbf{Ax}) )
+        + ( \mathbf{1} - \mathbf{b} )^T
+            \log( 1 - \sigma(\mathbf{Ax}) ) \right]
+    """
+
+    def __init__(self, A: NDArray[float64], b: NDArray[float64]):  # noqa: N803
+        if A.ndim != 2:
+            raise ValueError("Matrix A must be 2D")
+        if b.ndim != 1:
+            raise ValueError("Vector b must be 1D")
+        if A.shape[0] != b.shape[0]:
+            raise ValueError(f"Dimension mismatch: A has shape {A.shape} but b has length {b.shape[0]}")
+        class_labels = np.unique(b)
+        if class_labels.shape != (2,):
+            raise ValueError("Vector b must contain exactly two classes")
+        b = copy.deepcopy(b)
+        b[np.where(b == class_labels[0])], b[np.where(b == class_labels[1])] = 0, 1
+        self.A = A
+        self.b = b
+
+    @property
+    def domain_shape(self) -> tuple[int, ...]:  # noqa: D102
+        return (self.A.shape[1],)
+
+    @property
+    def m_smooth(self) -> float:
+        r"""
+        The cost function's smoothness constant.
+
+        .. math:: \frac{1}{4} \max_i \sum_{j} (\mathbf{A}_{ij})^2
+
+        For the general definition, see :attr:`CostFunction.m_smooth`.
+        """
+        return np.max(np.sum(self.A**2, axis=1, dtype=float64)) / 4
+
+    @property
+    def m_cvx(self) -> float:
+        """
+        The cost function's convexity constant, 0.
+
+        For the general definition, see :attr:`CostFunction.m_cvx`.
+        """
+        return 0
+
+    def evaluate(self, x: NDArray[float64]) -> float:
+        r"""
+        Evaluate function at x.
+
+        .. math::
+            -\left[ \mathbf{b}^T \log( \sigma(\mathbf{Ax}) )
+            + ( \mathbf{1} - \mathbf{b} )^T
+                \log( 1 - \sigma(\mathbf{Ax}) ) \right]
+        """
+        Ax = self.A.dot(x)  # noqa: N806
+        neg_log_sig = np.logaddexp(0.0, -Ax)
+        cost = self.b.dot(neg_log_sig) + (1 - self.b).dot(Ax + neg_log_sig)
+        return float(cost)
+
+    def gradient(self, x: NDArray[float64]) -> NDArray[float64]:
+        r"""
+        Gradient at x.
+
+        .. math:: \mathbf{A}^T (\sigma(\mathbf{Ax}) - \mathbf{b})
+        """
+        sig = special.expit(self.A.dot(x))
+        return self.A.T.dot(sig - self.b)  # type: ignore[no-any-return]
+
+    def hessian(self, x: NDArray[float64]) -> NDArray[float64]:
+        r"""
+        Hessian at x.
+
+        .. math:: \mathbf{A}^T \mathbf{DA}
+
+        where :math:`\mathbf{D}` is a diagonal matrix such that
+        :math:`\mathbf{D}_i = \sigma(\mathbf{Ax}_i) (1-\sigma(\mathbf{Ax}_i))`
+        """
+        sig = special.expit(self.A.dot(x))
+        D = np.diag(sig * (1 - sig))  # noqa: N806
+        return self.A.T.dot(D).dot(self.A)  # type: ignore[no-any-return]
+
+    def proximal(self, y: NDArray[float64], rho: float) -> NDArray[float64]:
+        """
+        Proximal at y solved using an iterative method.
+
+        See :meth:`CostFunction.proximal` for the general proximal definition.
+        """
+        prox_cf = ProximalCost(self, y, rho)
+        return ca.accelerated_gradient_descent(prox_cf, y, max_iter=100, stop_tol=1e-10, max_tol=None)
+
+
 class SumCost(CostFunction):
     """The sum of multiple cost functions."""
 
@@ -365,7 +462,7 @@ class SumCost(CostFunction):
         If any :math:`f_{k_\text{m_smooth}} = \text{NaN}`,
         the result is :math:`\text{NaN}`.
 
-        For the general definition, see :meth:`CostFunction.m_smooth`.
+        For the general definition, see :attr:`CostFunction.m_smooth`.
         """
         m_smooth_vals = [cf.m_smooth for cf in self.cost_functions]
         return np.nan if any(np.isnan(v) for v in m_smooth_vals) else sum(m_smooth_vals)
@@ -382,7 +479,7 @@ class SumCost(CostFunction):
         If any :math:`f_{k_\text{m_cvx}} = \text{NaN}`,
         the result is :math:`\text{NaN}`.
 
-        For the general definition, see :meth:`CostFunction.m_cvx`.
+        For the general definition, see :attr:`CostFunction.m_cvx`.
         """
         m_cvx_vals = [cf.m_cvx for cf in self.cost_functions]
         return np.nan if any(np.isnan(v) for v in m_cvx_vals) else sum(m_cvx_vals)
@@ -446,7 +543,7 @@ class ProximalCost(CostFunction):
 
         .. math:: f_\text{m_smooth} + \frac{1}{\rho}
 
-        For the general definition, see :meth:`CostFunction.m_smooth`.
+        For the general definition, see :attr:`CostFunction.m_smooth`.
         """
         return self.inner.m_smooth
 
@@ -457,7 +554,7 @@ class ProximalCost(CostFunction):
 
         .. math:: f_\text{m_cvx} + \frac{1}{\rho}
 
-        For the general definition, see :meth:`CostFunction.m_cvx`.
+        For the general definition, see :attr:`CostFunction.m_cvx`.
         """
         return self.inner.m_cvx
 
