e275e6674720e45d4041c6ab64d92ff300f4e4cf
Elias Ram <eliasframe@gmail.com>
2025-09-24 21:26:09 +0200

    ref: Flatten project structure

--- decent_bench/datasets/synthetic_classification_data.py
+++ decent_bench/datasets/synthetic_classification_data.py
@@ -6,24 +6,6 @@ from numpy import float64
 from numpy.typing import NDArray
 from sklearn import datasets as sk_datasets
 
-A = NewType("A", NDArray[float64])
-"""Feature matrix type."""
-
-b = NewType("b", NDArray[float64])
-"""Target vector type."""
-
-DatasetPartition = NewType("DatasetPartition", tuple[A, b])
-"""Tuple of (A, b) representing one dataset partition."""
-
-
-class Dataset(ABC):
-    """Dataset containing partitions in the form of feature matrix A and target vector b."""
-
-    @property
-    @abstractmethod
-    def training_partitions(self) -> Sequence[DatasetPartition]:
-        """Partitions used for finding the optimal optimization variable x."""
-
 
 class SyntheticClassificationData(Dataset):
     """
