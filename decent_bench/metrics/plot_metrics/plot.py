6964c79259db92291ee5c37066f546d75e2380b5
Elias Ram <eliasframe@gmail.com>
2025-09-25 19:50:37 +0200

    fix(metrics): Update doc links
    Update doc links shown to users, which were changed by the previous refactoring.

--- decent_bench/metrics/plot_metrics/plot.py
+++ decent_bench/metrics/plot_metrics/plot.py
@@ -15,7 +15,7 @@ from decent_bench.logger import LOGGER
 from decent_bench.metrics.plot_metrics.plot_metrics_constructs import PlotMetric, X, Y
 from decent_bench.network import Network
 
-DOC_LINK = "https://decent-bench.readthedocs.io/en/latest/decent_bench.library.core.metrics.plot_metrics.html"
+DOC_LINK = "https://decent-bench.readthedocs.io/en/latest/decent_bench.metrics.plot_metrics.html"
 COLORS = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd", "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
 MARKERS = ["o", "s", "v", "^", "*", "D", "H", "<", ">", "p"]
 
