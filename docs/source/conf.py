45b8cbc0c185694ee000730784ea291e46322b6b
elramen <158566966+elramen@users.noreply.github.com>
2025-10-01 09:10:16 +0200

    docs(manual): Update copyright (#130)
    Update copyright to Team Decent instead of team-decent to align with the
    organization's public name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -7,7 +7,7 @@
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
 project = "decent-bench"
-copyright = "2025, team-decent"
+copyright = "2025, Team Decent"
 author = "team-decent"
 release = "0.0.0"
 
