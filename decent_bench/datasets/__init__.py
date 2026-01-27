bec7595884fdc4f42e3b984f9874b7e0f2c3bf02
simpag <simon.granstrom@gmail.com>
2026-01-27 18:03:17 +0100

    fix(Datasets): Include pytorch and kaggle datasets into api

--- decent_bench/datasets/__init__.py
+++ decent_bench/datasets/__init__.py
@@ -1,8 +1,12 @@
 from ._dataset import Dataset, DatasetPartition
+from ._kaggle_dataset import KaggleDataset
+from ._pytorch_dataset import PyTorchWrapper
 from ._synthetic_classification import SyntheticClassificationData
 
 __all__ = [
     "Dataset",
     "DatasetPartition",
+    "KaggleDataset",
+    "PyTorchWrapper",
     "SyntheticClassificationData",
 ]
