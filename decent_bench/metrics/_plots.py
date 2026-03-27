0d22f7d580c8bb176a470d3111e084e36d358160
simpag <simon.granstrom@gmail.com>
2026-03-27 22:00:58 +0100

    fix(Plot): Update warning message

--- decent_bench/metrics/_plots.py
+++ decent_bench/metrics/_plots.py
@@ -247,15 +247,17 @@ def _create_and_plot_figures(
                     non_positive_replacement = min(*(val for val in x if val > 0), 1e-8)
                     x = [val if val > 0 else non_positive_replacement for val in x]  # avoid log(0) issues
                     LOGGER.warning(
-                        f"Metric '{metric.plot_description}' has x_log=True but contains non-positive x values. "
-                        f"These values have been replaced with {non_positive_replacement} for plotting purposes."
+                        f"Metric '{metric.plot_description}' has x_log=True but contains non-positive x values for"
+                        f"algorithm {alg.name}. These values have been replaced with {non_positive_replacement} for "
+                        f"plotting purposes."
                     )
                 if metric.y_log and any(val <= 0 for val in y_mean):
                     non_positive_replacement = min(*(val for val in y_mean if val > 0), 1e-8)
                     y_mean = [val if val > 0 else non_positive_replacement for val in y_mean]
                     LOGGER.warning(
-                        f"Metric '{metric.plot_description}' has y_log=True but contains non-positive y values. "
-                        f"These values have been replaced with {non_positive_replacement} for plotting purposes."
+                        f"Metric '{metric.plot_description}' has y_log=True but contains non-positive y values for"
+                        f"algorithm {alg.name}. These values have been replaced with {non_positive_replacement} for "
+                        f"plotting purposes."
                     )
                 if metric.y_log and any(val <= 0 for val in y_min):
                     non_positive_replacement = min(*(val for val in y_min if val > 0), 1e-8)
